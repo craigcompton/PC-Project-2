d642317d6c945ff6bd7641a362cc4e6d45d92796
craigcompton <39470012+craigcompton@users.noreply.github.com>
2018-11-28 20:50:09 -0800

    added some db info

---
+++ models/reminder.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+  var Reminder = sequelize.define("Reminder", {
+    title: DataTypes.TEXT,
+    time: DataTypes.TEXT,
+    alarmType: DataTypes.TEXT,
+    repeatFreq: DataTypes.TEXT,
+    repeatDays: DataTypes.TEXT
+  });
+  return Reminder;
+};
---
+++ models/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS reminders_db;
+CREATE DATABASE reminders_db;
+
+-- DROP DATABASE IF EXISTS testdb;
+-- CREATE DATABASE testdb;
