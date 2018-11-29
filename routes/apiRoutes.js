dd4635c453608222cc7c09753be3dae2b235896d
craigcompton <39470012+craigcompton@users.noreply.github.com>
2018-11-29 11:17:27 -0800

    Add files via upload
    added more html and api routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,31 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all reminders
+  app.get("/api/reminders", function(req, res) {
+    db.Example.findAll({}).then(function(dbreminders) {
+      res.json(dbreminders);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/reminders", function(req, res) {
+    db.Example.create(req.body).then(function(dbreminders) {
+      res.json(dbreminders);
     });
   });
 
     // Update an example
-    app.post("/api/examples", function(req, res) {
-      db.Example.update(req.body).then(function(dbExample) {
-        res.json(dbExample);
+    app.post("/api/reminders", function(req, res) {
+      db.Example.update(req.body).then(function(dbreminders) {
+        res.json(dbreminders);
       });
     });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/reminders/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbreminders) {
+      res.json(dbreminders);
     });
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Reminder.findAll({}).then(function(dbReminders) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        Reminders: dbReminders
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load Reminder page and pass in an Reminder by id
+  app.get("/Reminder/:id", function(req, res) {
+    db.Reminder.findOne({ where: { id: req.params.id } }).then(function(dbReminders) {
+      res.render("Reminder", {
+        Reminder: dbReminders
       });
     });
   });
