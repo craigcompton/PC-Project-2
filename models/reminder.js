b6be4a56ed200133df2630875025e84fd9706774
craigcompton <39470012+craigcompton@users.noreply.github.com>
2018-11-29 11:27:33 -0800

    Add files via upload
    more routes!

--- models/reminder.js
+++ models/reminder.js
@@ -2,9 +2,9 @@ module.exports = function(sequelize, DataTypes) {
   var Reminder = sequelize.define("Reminder", {
     title: DataTypes.TEXT,
     time: DataTypes.TEXT,
-    alarmType: DataTypes.TEXT,
-    repeatFreq: DataTypes.TEXT,
-    repeatDays: DataTypes.TEXT
+    alarmType: DataTypes.TEXT//,
+    // repeatFreq: DataTypes.TEXT,
+    // repeatDays: DataTypes.TEXT
   });
   return Reminder;
 };
